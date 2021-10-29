6f751b28260e9db600ac7d50f3af5c82c638a753
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-10-29 11:24:44 -0500

    cmabio de hoy

--- website_route/productos/models.py
+++ website_route/productos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto (models.Model):
+    descripcion=models.CharField(max_length=50)
+    presentacion=models.CharField(max_length=50)
+    precio=models.FloatField()
+
+
+    def __str__(self):
+        return self.descripcion
